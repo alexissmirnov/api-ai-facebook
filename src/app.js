bbcadaca76e8695d6a4d9953c8395d7a8b669cf5
alexis@smirnov.ca <alexissmirnov>
2017-06-18 10:58:35 -0400

    test

--- src/app.js
+++ src/app.js
@@ -9,10 +9,10 @@ const JSONbig = require('json-bigint');
 const async = require('async');
 
 const REST_PORT = (process.env.PORT || 5000);
-const APIAI_ACCESS_TOKEN = process.env.APIAI_ACCESS_TOKEN;
+const APIAI_ACCESS_TOKEN = process.env.APIAI_ACCESS_TOKEN || '823143bc340b4f48854be82df98bf288';
 const APIAI_LANG = process.env.APIAI_LANG || 'en';
-const FB_VERIFY_TOKEN = process.env.FB_VERIFY_TOKEN;
-const FB_PAGE_ACCESS_TOKEN = process.env.FB_PAGE_ACCESS_TOKEN;
+const FB_VERIFY_TOKEN = process.env.FB_VERIFY_TOKEN || 'EAADqzD3Q6tsBANkBeFudzeT8IYZCxD2yva7gMZAZCRZASGR3mqW3yXL9ZANl5UMSdTfxxdz4jdZBqtteDLbzXURDKnWoWvqgCrpf85NlszxPtXG5cbGI4bfdYVFqwtp1R0irLZAAB6rDYIc42clhmceanoWbZCMio6AhfQZBIDGIOZBgZDZD';
+const FB_PAGE_ACCESS_TOKEN = process.env.FB_PAGE_ACCESS_TOKEN || 'EAADqzD3Q6tsBANkBeFudzeT8IYZCxD2yva7gMZAZCRZASGR3mqW3yXL9ZANl5UMSdTfxxdz4jdZBqtteDLbzXURDKnWoWvqgCrpf85NlszxPtXG5cbGI4bfdYVFqwtp1R0irLZAAB6rDYIc42clhmceanoWbZCMio6AhfQZBIDGIOZBgZDZD';
 const FB_TEXT_LIMIT = 640;
 
 const FACEBOOK_LOCATION = "FACEBOOK_LOCATION";
